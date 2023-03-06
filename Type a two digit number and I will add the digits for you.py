c7dc4c0b94f31d0ac5235530e04d27622bedcfbe
Kamil Wiśniewski <124616552+kwisniewski0103@users.noreply.github.com>
2023-03-06 09:45:08 +0100

    Add files via upload

---
+++ Type a two digit number and I will add the digits for you.py
@@ -0,0 +1,8 @@
+# 2. Type a two digit number and I will add the digits for you:
+
+two_digit_number = input("Type a two digit number and I will add the digits for you: ")
+first_digit = int(two_digit_number[0]) 
+second_digit = int(two_digit_number[1])
+two_digits = first_digit + second_digit
+print(two_digits)
+
